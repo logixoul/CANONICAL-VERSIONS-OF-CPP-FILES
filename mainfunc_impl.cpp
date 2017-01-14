e82fa97b3b39f7f2dd2e808b09d4892086745f1d
Stefan Monov <logixoul@gmail.com>
2017-01-14 17:03:34 +0200

    fix buggy code

--- mainfunc_impl.cpp
+++ mainfunc_impl.cpp
@@ -1,11 +1,11 @@
 #include "precompiled.h"
-#include "app.h"
+#include <cinder/app/AppBasic.h>
+#include "util.h"
 
 int mainFuncImpl(cinder::app::AppBasic* app) {
 	try {
 		createConsole();
 		cinder::app::AppBasic::prepareLaunch();														
-		cinder::app::AppBasic *app = new SApp;														
 		cinder::app::Renderer *ren = new RendererGl;													
 		cinder::app::AppBasic::executeLaunch( app, ren, "SApp" );										
 		cinder::app::AppBasic::cleanupLaunch();														
